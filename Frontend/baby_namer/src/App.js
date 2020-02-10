cecd838ceb5283f42ba890efe5630d724a8230af
github username <github email address>
2020-02-10 14:43:17 -0500

    sample info

--- Frontend/baby_namer/src/App.js
+++ Frontend/baby_namer/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import NameContainer from './NameContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+
+  state = {
+    allNames: [{ id: 1, name: "Bob"},{ id: 2, name: "Jack"},{ id: 3, name: "Jill"},{ id: 4, name: "Job"},{ id: 5, name: "Back"},{ id: 6, name: "Bill"},{ id: 7, name: "Kobe"}]
+  }
+
+  // componentDidMount() {
+  // fetch('localhost:3000/babynames')
+  // .then(res => res.json())
+  // .then(data => this.setState({allNames: data}));
+  // }
+
+  render() {
+
+    return (
+      <div>
+        <Header/>
+        <NameContainer allNames={this.state.allNames}/>
+      </div>
+
+
+    )
+  }
 }
-
-export default App;
---
+++ Frontend/baby_namer/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Search from 'semantic'
+export default class Header extends React.Component {
+
+    render() {
+
+        return (
+            <center>
+                <h1>BABY NAMER</h1>
+                
+            </center>
+        )
+    }
+}
