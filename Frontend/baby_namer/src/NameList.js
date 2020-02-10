e4e516f0f1302c92cdf66754249ab4366e1754c5
github username <github email address>
2020-02-10 12:47:06 -0500

    empty backend

---
+++ Frontend/baby_namer/src/NameList.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+
+// export default class NameList extends React.Component {
+
+//     renderNames = () => {
+
+//     }
+
+// render() {
+
+//     return (
+
+
+//     )
+// }
+
+
+// }
